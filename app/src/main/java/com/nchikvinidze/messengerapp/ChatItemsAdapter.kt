56ec472b1232d840d9c1ba2b40e68f7eb63d4972
bubu <nchik12@freeuni.edu.ge>
2021-07-24 10:53:06 +0400

    chat beginnings

---
+++ app/src/main/java/com/nchikvinidze/messengerapp/ChatItemsAdapter.kt
@@ -0,0 +1,25 @@
+package com.nchikvinidze.messengerapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ChatItemsAdapter() : RecyclerView.Adapter<ChatItemViewHolder>()  {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ChatItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class ChatItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
